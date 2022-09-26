a69c06a4b93f092f72f4168c784b1a0971bdfcb7
WTPENGUIN <ccho2932@naver.com>
2022-09-26 22:54:59 +0900

    update action script

--- scripts/update_generated_repos.sh
+++ scripts/update_generated_repos.sh
@@ -2,8 +2,8 @@
 
 # Config for auto-building
 git remote rename origin upstream
-git config --global user.email "bot@pixhawk.org"
-git config --global user.name "PX4BuildBot"
+git config --global user.email "ccho2932@naver.com"
+git config --global user.name "WTPENGUIN"
 git config --global credential.helper "store --file=$HOME/.git-credentials"
 echo "https://${GH_TOKEN}:@github.com" > "$HOME"/.git-credentials
 
@@ -15,9 +15,9 @@ git clone https://github.com/mavlink/c_library_v2.git
 cd ../../..
 ./scripts/update_c_library.sh 2
 # v1.0 legacy
-cd "$GEN_START_PATH"
-mkdir -p include/mavlink/v1.0
-cd include/mavlink/v1.0
-git clone https://github.com/mavlink/c_library_v1.git
-cd ../../..
-./scripts/update_c_library.sh 1
+#cd "$GEN_START_PATH"
+#mkdir -p include/mavlink/v1.0
+#cd include/mavlink/v1.0
+#git clone https://github.com/mavlink/c_library_v1.git
+#cd ../../..
+#./scripts/update_c_library.sh 1
